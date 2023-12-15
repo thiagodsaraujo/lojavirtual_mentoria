825013e0fa6cae684bdb83d679c82a33f8993621
Thiago Araujo <thiago.dos.araujo@maisunifacisa.com.br>
2023-12-15 17:11:55 -0300

    first commit

---
+++ src/main/java/dev/mentoria/lojavirtual_mentoria/LojavirtualMentoriaApplication.java
@@ -0,0 +1,13 @@
+package dev.mentoria.lojavirtual_mentoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojavirtualMentoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojavirtualMentoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mentoria/lojavirtual_mentoria/LojavirtualMentoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mentoria.lojavirtual_mentoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojavirtualMentoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
