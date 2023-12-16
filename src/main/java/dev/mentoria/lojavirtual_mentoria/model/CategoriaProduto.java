e9e5ea0394eae877e2b32afb01a3f6da82874764
Thiago Araujo <thiago.dos.araujo@maisunifacisa.com.br>
2023-12-16 10:29:45 -0300

    Criando Marca e Categoria Produto

---
+++ src/main/java/dev/mentoria/lojavirtual_mentoria/model/CategoriaProduto.java
@@ -0,0 +1,48 @@
+package dev.mentoria.lojavirtual_mentoria.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categoria_produto")
+public class CategoriaProduto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+
+    @Column(nullable = false)
+    private String nomeDesc;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeDesc() {
+        return nomeDesc;
+    }
+
+    public void setNomeDesc(String nomeDesc) {
+        this.nomeDesc = nomeDesc;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CategoriaProduto that = (CategoriaProduto) o;
+
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getId().hashCode();
+    }
+}
---
+++ src/main/java/dev/mentoria/lojavirtual_mentoria/model/MarcaProduto.java
@@ -0,0 +1,54 @@
+package dev.mentoria.lojavirtual_mentoria.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "marca_produto")
+public class MarcaProduto implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+
+    @Column(nullable = false)
+    private String nomeDesc;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeDesc() {
+        return nomeDesc;
+    }
+
+    public void setNomeDesc(String nomeDesc) {
+        this.nomeDesc = nomeDesc;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MarcaProduto that = (MarcaProduto) o;
+
+        return getId() != null ? getId().equals(that.getId()) : that.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
+}
