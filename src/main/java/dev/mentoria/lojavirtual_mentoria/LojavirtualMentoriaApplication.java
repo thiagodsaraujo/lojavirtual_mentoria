6147e25d0d9340bafdb0b3491f37bdea57f7051c
Thiago Araujo <thiago.dos.araujo@maisunifacisa.com.br>
2023-12-24 16:18:22 -0300

    Criando o v1 do bd, configurando application.properties e anotações na classe principal do spring

--- src/main/java/dev/mentoria/lojavirtual_mentoria/LojavirtualMentoriaApplication.java
+++ src/main/java/dev/mentoria/lojavirtual_mentoria/LojavirtualMentoriaApplication.java
@@ -3,8 +3,15 @@ package dev.mentoria.lojavirtual_mentoria;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = "dev.mentoria.lojavirtual_mentoria.model")
+@ComponentScan(basePackages = {"dev.*"}) // procura pelos recursos no array dentro das pastas dev
+@EnableJpaRepositories(basePackages = {"dev.mentoria.lojavirtual_mentoria.repository"}) //vai procurar os repositories nessa pasta em especifico
+@EnableTransactionManagement
 public class LojavirtualMentoriaApplication {
 
 	public static void main(String[] args) {
